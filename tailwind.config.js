805585b84737872c42eec26be997903733065be5
MatthewSaintbull <m.santoro1997@gmail.com>
2022-11-27 11:57:28 +0100

    feat: add fading presentation + remove non used animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,23 +14,9 @@ module.exports = {
           from: { opacity: 1 },
           to: { opacity: 0 },
         },
-        fadein: {
-          from: { opacity: 0 },
-          to: { opacity: 1 }
-        },
-        expand: {
-          '0%': {
-            transform: 'scale(1,0)',
-          },
-          '100%': {
-            transform: 'scale(1,1)',
-          }
-        }
       },
       animation: {
         fadeout: 'fadeout 3s forwards;',
-        fadein: 'fadein 5s forwards',
-        expand: 'expand 2s linear forwards'
       }
     },
   },
