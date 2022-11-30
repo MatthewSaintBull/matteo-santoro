39080caaaf90a9bb492307d7cc45b96c33033ed8
MatthewSaintbull <m.santoro1997@gmail.com>
2022-11-30 18:06:58 +0100

    feat: add bio + add navbar

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,9 @@ import Head from 'next/head'
 import FadingPresentation from '../components/fadingPresentation'
 import CurriculumComponent from '../components/curriculumComponent'
 import FooterComponent from '../components/footer'
-import { useEffect, useRef, useState } from 'react'
+
 
 export default function Home() {
-  const mainContainer = useRef(null)
-  const fadingRef = useRef(null)
-  const [screenSize, setScreenSize] = useState(false)
-
-  useEffect(() => {
-    if (mainContainer?.current?.clientHeight > 30) {
-      setScreenSize(true)
-    }
-  }, [fadingRef])
-
   return (
     <div>
       <Head>
@@ -22,11 +12,11 @@ export default function Home() {
         <meta name="description" content="Matteo Santoro CV" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='w-full h-full' ref={mainContainer}>
-        <FadingPresentation ref={fadingRef} />
-        {screenSize && <div className='max-w-7xl mx-auto overflow-auto' >
+      <main className='w-full h-full'>
+        <FadingPresentation/>
+        <div className='max-w-7xl mx-auto overflow-auto' >
           <CurriculumComponent />
-        </div>}
+        </div>
       </main>
       <footer>
         <FooterComponent />
